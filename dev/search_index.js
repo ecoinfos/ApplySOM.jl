172a177066e1d27ff28532fd6c5c7a822710f835
Documenter.jl <documenter@juliadocs.github.io>
2023-11-18 09:08:19 +0000

    build based on 71913f5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ApplySOM","category":"page"},{"location":"#ApplySOM","page":"Home","title":"ApplySOM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ApplySOM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ApplySOM]","category":"page"}]
+}
